fc28119b2c28d0ee79a2412ae2d3e899f31b0df4
Lymbo49 <elymberopoulos@luc.edu>
2018-03-22 18:41:11 -0500

    Client test

---
+++ osc-project-6/Exchange/src/osdi/client/test/ClientTest.java
@@ -0,0 +1,18 @@
+package osdi.client.test;
+import org.junit.Assert;
+
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClientTest {
+
+	@Test
+	public void test() {
+		assertTrue("Not yet implemented", true);
+		
+	}
+
+}
