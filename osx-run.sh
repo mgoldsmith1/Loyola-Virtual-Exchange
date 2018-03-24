4749ed2216ae240f67f5d132e8a7ed027f586a69
Matthew Goldsmith <mgoldsmith@github.com>
2018-03-24 01:40:44 -0500

    modified osx-run.sh

--- osx-run.sh
+++ osx-run.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
 
+
 cd osc-project-6/Exchange/jars
-java -jar server.jar | osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' | java -jar client.jar
+ 
+java -jar server.jar | osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' -e 'tell application "System Events" to tell process "Terminal" to keystroke "cd osc-project-6/Exchange/jars" ' -e 'tell application "System Events" to tell process "Terminal" to keystroke "\r" ' -e 'tell application "System Events" to tell process "Terminal" to keystroke "java -jar client.jar" ' -e 'tell application "System Events" to tell process "Terminal" to keystroke "\r" '
+
+
+#-e 'tell application "System Events" to tell process "Terminal" to keystroke "Finder" '
+
+#------------------------------------------------------------
+#pwd > destination
+#sed 's/^/cd /' destination
+
+
+#echo destination | sed 's/here/cd /' torun.sh > torun.sh
+#sed 's/here/cd '|pwd|'/' osx-run.sh #myrun.sh
+#mv destination osc-project-6/Exchange/jars
+#cd osc-project-6/Exchange/jars
+
+#./torun.sh
+
+#sed 's/here/'|cat destination|'/' torun.sh
