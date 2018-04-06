5aea3aa541048643c89576f89767b0ce6996bb75
Matthew Goldsmith <mgoldsmith@github.com>
2018-04-06 14:10:53 -0500

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-cd osc-project-6/Exchange/jars
+cd osc-project-6
 echo ‘Starting Client…’
-java -jar client.jar
+java -jar client.jar 
 
 #echo ‘Starting Server…’
 #need a ordermatch jar with a pipeline to client jar
