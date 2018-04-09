4631af8fe44e94db335a52c53ef0e3c33be34460
Matthew Goldsmith <31556412+mgoldsmith1@users.noreply.github.com>
2018-04-09 09:20:23 -0500

    Update install-run.sh

--- install-run.sh
+++ install-run.sh
@@ -5,17 +5,60 @@
 # reinstall the latest version:
 # brew cask install caskroom/versions/java
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#Checking Version of Java
+if type -p java; then
+    echo found java executable in PATH
+    _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+    echo found java executable in JAVA_HOME     
+    _java="$JAVA_HOME/bin/java"
+else
+    echo "no java"
+fi
 
-brew update
-brew tap caskroom/cask
-brew cask install caskroom/versions/java8
+if [[ "$_java" ]]; then
+    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    echo version "$version"
+    if [[ "$version" > "1.8" ]] && [[ "$version" < "1.9" ]]; then
+         echo Your Version of Java is more than 1.8 but less than 1.9
+	 echo No need to Upgrade... Skipping Installation Step
 
-java -version
+	 #Cloning
+	 git clone https://github.com/mgoldsmith1/Loyola-Virtual-Exchange.git
+	 cd Loyola-Virtual-Exchange
 
-git clone https://github.com/mgoldsmith1/Loyola-Virtual-Exchange.git
-cd Loyola-Virtual-Exchange
+	 #Running Application
+	 echo Running Application... 
+	 ./run.sh
+    elif [[ "$version" > "1.9" ]]; then        
+         echo Your Version of Java is greater than 1.9
 
-./run.sh
+         #Cloning	
+         git clone https://github.com/mgoldsmith1/Loyola-Virtual-Exchange.git
+	 cd Loyola-Virtual-Exchange
+
+	 #Running Application
+	 echo Running Application...
+	 ./run.sh
+    else 
+ 	 echo Your Version of Java is less than 1.8
+	 echo Upgrading to Java 8...
+
+	 #With Homebrew we will install Java 8
+	 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	 brew update
+	 brew tap caskroom/cask
+	 brew cask install caskroom/versions/java8
+
+	 #Cloning
+	 echo Cloning...
+	 git clone https://github.com/mgoldsmith1/Loyola-Virtual-Exchange.git
+	 cd Loyola-Virtual-Exchange
+
+	 #Running Application
+	 echo Running Application...
+	 ./run.sh
+    fi
+fi
 
 
