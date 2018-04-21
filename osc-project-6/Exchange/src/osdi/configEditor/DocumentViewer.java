b40a04c418fb86a7cbe2073be4e1c1e532f8bdc6
Lymbo49 <elymberopoulos@luc.edu>
2018-04-20 20:09:05 -0500

    Jfilechooser now is initiated to a more accurate directory.

--- osc-project-6/Exchange/src/osdi/configEditor/DocumentViewer.java
+++ osc-project-6/Exchange/src/osdi/configEditor/DocumentViewer.java
@@ -1,48 +1,97 @@
 package osdi.configEditor;
 
-
 import java.awt.BorderLayout;
 import java.awt.event.*;
 import javax.swing.*;
-import java.io.File;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class DocumentViewer {
+	private Path path;
 
 	public DocumentViewer() {
+		URL resourseURL = null;
+		
+		try {
+		    resourseURL = getClass().getResource("FIXTracker.cfg");
+			File file = null;
+			
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
 
-	    
-	                final JFrame f = new JFrame("Document Viewer");
-	                f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		final JFrame f = new JFrame("Document Viewer");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		final JFileChooser fileChooser = new JFileChooser();
+		
+		try {
+			fileChooser.setCurrentDirectory(new java.io.File( resourseURL.toURI()));
+		} catch (URISyntaxException e1) {
+			e1.printStackTrace();
+		}
+		JPanel gui = new JPanel(new BorderLayout());
 
-	                final JFileChooser fileChooser = new JFileChooser();
+		final JEditorPane document = new JEditorPane();
+		gui.add(new JScrollPane(document), BorderLayout.CENTER);
+		
+		URL classpath = DocumentViewer.class.getResource("DocumentViewer.class");
+		File location = new File("locationTarget");
+	
 
-	                JPanel gui = new JPanel(new BorderLayout());
+		fileChooser.setDialogTitle("Configuration Files Only");
+		fileChooser.setFileFilter(new CFGSearcher());
 
-	                final JEditorPane document = new JEditorPane();
-	                gui.add(new JScrollPane(document), BorderLayout.CENTER);
+		JButton open = new JButton("Open");
+		JButton save = new JButton("Save");
 
-	                JButton open = new JButton("Open");
-	                open.addActionListener( new ActionListener() {
-	                    public void actionPerformed(ActionEvent ae) {
-	                        int result = fileChooser.showOpenDialog(f);
-	                        if (result==JFileChooser.APPROVE_OPTION) {
-	                            File file = fileChooser.getSelectedFile();
-	                            try {
-	                                document.setPage(file.toURI().toURL());
-	                            } catch(Exception e) {
-	                                e.printStackTrace();
-	                            }
-	                        }
-	                    }
-	                });
-	                gui.add(open, BorderLayout.NORTH);
+		open.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				int result = fileChooser.showOpenDialog(f);
+				if (result == JFileChooser.APPROVE_OPTION) {
+					File file = fileChooser.getSelectedFile();
+					try {
+						document.setPage(file.toURI().toURL());
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
 
-	                f.setContentPane(gui);
-	                f.pack();
-	                f.setSize(400,300);
-	                f.setLocationByPlatform(true);
+		save.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				int result = fileChooser.showSaveDialog(f);
+				if (result == JFileChooser.APPROVE_OPTION) {
+					File file = fileChooser.getSelectedFile();
+					try {
+						document.setPage(file.toURI().toURL());
+						BufferedWriter bWriter = null;
+						if(result == JFileChooser.APPROVE_OPTION) {
+							bWriter = new BufferedWriter(new FileWriter(file));
+							document.write(bWriter);
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
 
-	                f.setVisible(true);
-	            }
-	    }
+		gui.add(open, BorderLayout.NORTH);
+		gui.add(save, BorderLayout.SOUTH);
+
+		f.setContentPane(gui);
+		f.pack();
+		f.setSize(400, 300);
+		f.setLocationByPlatform(true);
+
+		f.setVisible(true);
+	}
+}
